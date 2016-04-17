5ec69b00b9b28c83c151892a319b111c13eec30b
Daniel Dao <daniel.dddao@gmail.com>
2016-04-16 20:20:43 -0500

    lambda terms parsing

--- src/main/java/lambdaCalculus/LambdaDef.java
+++ src/main/java/lambdaCalculus/LambdaDef.java
@@ -1,18 +1,152 @@
 package lambdaCalculus;
 
+import lambdaCalculus.Tree.Tree;
+import lambdaCalculus.Tree.TreePrinter;
+
+import java.util.LinkedList;
+import java.util.jar.Pack200;
+
 /**
  * Created by Daniel Dao on 4/15/16.
  */
 public class LambdaDef {
-    static String lambda = "λ";
+
+    private final char lambda = 'λ';
+    private final char app = '@';
+
+    private Tree<Character> lambdaTree;
+    private String lambdaTerm;
+
+    public LambdaDef(String input) {
+
+        try {
+
+            // check the input format
+            if (!isLambdaTerm(input))
+                throw new Exception("Error! Given term is not in correct format!\n" + input);
+
+            // Replace backslash with λ
+            setLambdaTerm(input.replace("\\", "" + lambda));
+
+            // Convert the lambdaTerm to lambdaTree
+             setLambdaTree(toTree(lambdaTerm));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Tree<Character> getLambdaTree() {
+        return lambdaTree;
+    }
+
+    private void setLambdaTree(Tree<Character> lambdaTree) {
+        this.lambdaTree = lambdaTree;
+    }
 
     /*
-     * Check if the given lambda term is in correct format
+     * Get the lambda term
      */
-    public boolean termChecker(String lambdaTerm) {
-        boolean isCorrectTerm = false;
+    public String getLambdaTerm() {
+        return lambdaTerm;
+    }
 
-        return isCorrectTerm;
+    /*
+     * Set the lambda term
+     */
+    private void setLambdaTerm(String lambdaTerm) {
+        this.lambdaTerm = lambdaTerm;
+    }
+
+    /*
+     * Check if the given input is in valid format
+     */
+    public boolean isLambdaTerm(String input) {
+        boolean result = true;
+
+        try {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    private int findTermInParens(String term) {
+        int index = -1;
+        LinkedList<Character> list = new LinkedList<Character>();
+
+        try {
+            char[] charTerm = term.toCharArray();
+            for (int x=0; x < charTerm.length; x++) {
+                if (charTerm[x] == '(')
+                    list.add(charTerm[x]);
+                else if (charTerm[x] == ')')
+                    list.remove();
+                if (list.size() == 0)
+                    return x;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return index;
+    }
+
+    /*
+     * Convert the standard lambda term to a tree.
+     */
+    private Tree<Character> toTree (String term) {
+        Tree<Character> tree = null;
+        try {
+            char firstLetter = term.charAt(0);  // first letter of the term
+
+            if (firstLetter == '(') {   // if the first letter is "("
+                int index = findTermInParens(term);
+                if(index > 0) {
+                    if (index == term.length()-1) {
+                        term = term.substring(1, index);
+                        tree = toTree(term);
+                    } else {
+                        Tree<Character> leftChild = toTree(term.substring(1, index));
+                        term = term.substring(index + 1);
+                        if (term.charAt(0) == ' ') {
+                            term = term.substring(1);
+                            tree = new Tree<Character>(app, leftChild, toTree(term));
+                        }
+                    }
+                }
+
+            } else if (firstLetter == lambda) { // if the first letter is a λ
+
+                term = term.substring(1);
+                int dot = term.indexOf('.');
+                char var = term.charAt(0);
+                term = term.substring(dot + 1);
+                Tree<Character> leftChild = new Tree<Character>(var, null, null);
+                tree = new Tree<Character>(lambda, leftChild, toTree(term));
+
+            } else if ((firstLetter >= 'a' && firstLetter <= 'z') || (firstLetter >= 'A' && firstLetter <= 'Z')) {
+
+                int whiteSpace = term.indexOf(' ');
+                if (whiteSpace > 0) {
+                    char var = term.charAt(0);
+                    term = term.substring(whiteSpace + 1);
+
+                    Tree<Character> leftChild = new Tree<Character>(var, null, null);
+                    tree = new Tree<Character>(app, leftChild, toTree(term));
+                } else {
+                    tree = new Tree<Character>(firstLetter, null, null);
+                }
+            }
+
+            //System.out.println("Term: " + term);
+            //System.out.println("List: " + list);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return tree;
     }
 
     /*
@@ -20,6 +154,12 @@ public class LambdaDef {
      */
     public String lambdaConvert(String input) {
         String term = "";
+
+        try {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return term;
     }
--- src/main/java/lambdaCalculus/Runner.java
+++ src/main/java/lambdaCalculus/Runner.java
@@ -1,10 +1,28 @@
 package lambdaCalculus;
 
+
+import lambdaCalculus.Tree.TreePrinter;
+
 /**
  * Created by Daniel Dao on 4/16/16.
  */
 public class Runner {
+
+    public static void print(String input) {
+        LambdaDef lambdaDef = new LambdaDef(input);
+        String term = lambdaDef.getLambdaTerm();
+        System.out.println("Tree for " + term + " :");
+        TreePrinter.printNode(lambdaDef.getLambdaTree());
+    }
+
     public static void main(String[] args) {
-        System.out.println(new TermsConverter().deBruijnToStandardLambda(""));
+        String input1 = "(\\x.\\y.(x y) z)";
+        String input2 = "\\x.\\y.x y z";
+        String input3 = "x";
+
+        print(input1);
+        print(input2);
+        //print(input3);
+
     }
 }
